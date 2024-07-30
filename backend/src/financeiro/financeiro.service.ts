6a99d4904efb97720a44e846c3ecf7012c00669e
Nicolas Klaumann <nicolas@conectra.com.br>
2024-07-30 13:51:08 -0300

    feat: ajusta intervalo entre o contrato e a parcela e busca o maior valor

--- backend/src/financeiro/financeiro.service.ts
+++ backend/src/financeiro/financeiro.service.ts
@@ -10,24 +10,49 @@ export class FinanceiroService {
 
     // inicializa prop para controle da logica
     let tempo = [];
+    let saldo = 0.0;
 
     // iterar sobre os contratos e atualizar gradativamente o saldo
     data.contratos.map((contrato) => {
       // parse em cima da data para nomear a posição do array com o mes da movimentação
       const [anoContrato, mesContrato, diaContrato] = contrato.data.split('-');
 
+      // controle de saldo
+      saldo += contrato.valorfinanciado;
+
       // cria ou atualiza o saldo do mes
       if (!tempo[`${mesContrato}/${anoContrato}`]) {
         tempo[`${mesContrato}/${anoContrato}`] = {
-          saldoDevedor: contrato.valortotal - contrato.valorentrada,
+          saldoDevedor: saldo,
         };
       } else {
-        tempo[`${mesContrato}/${anoContrato}`].saldoDevedor +=
-          contrato.valortotal - contrato.valorentrada;
+        tempo[`${mesContrato}/${anoContrato}`].saldoDevedor += saldo;
       }
 
-      // prop para controlar o valor financiado nas parcelas
-      let valorFinanciado = contrato.valorfinanciado;
+      // se a data de vencimento da primeira parcela for mais de 1 mes depois do inicio do contrato, é necessario colocar a divida nesse espaço de tempo
+      const mesesSemInteracao = differenceInMonths(
+        new Date(contrato.data),
+        new Date(contrato.parcelas[0].datavencimento),
+      );
+
+      if (mesesSemInteracao >= 2) {
+        for (let i = 1; i <= mesesSemInteracao - 1; i++) {
+          const novaData = new Date(addMonths(new Date(contrato.data), i));
+
+          const [anoNovaData, mesNovaData, diaNovaData] = novaData
+            .toISOString()
+            .split('T')[0]
+            .split('-');
+          if (tempo[`${mesNovaData}/${anoNovaData}`]) {
+            tempo[`${mesNovaData}/${anoNovaData}`].saldoDevedor +=
+              contrato.valorfinanciado;
+          } else {
+            tempo[`${mesNovaData}/${anoNovaData}`] = {
+              saldoDevedor: contrato.valorfinanciado,
+            };
+          }
+        }
+      }
 
       // calculo do pagamanento do contrato
       contrato.parcelas.map((parcela) => {
@@ -43,11 +68,10 @@ export class FinanceiroService {
             new Date(parcela.datavencimento).getMonth()
         ) {
           if (tempo[`${mesVencimento}/${anoVencimento}`]) {
-            tempo[`${mesVencimento}/${anoVencimento}`].saldoDevedor +=
-              valorFinanciado;
+            tempo[`${mesVencimento}/${anoVencimento}`].saldoDevedor += saldo;
           } else {
             tempo[`${mesVencimento}/${anoVencimento}`] = {
-              saldoDevedor: valorFinanciado,
+              saldoDevedor: saldo,
             };
           }
 
@@ -60,20 +84,20 @@ export class FinanceiroService {
         const [anoParcela, mesParcela, diaParcela] =
           parcela.dataultimopagamento.split('-');
 
-        // atualiza valor financiado
-        valorFinanciado -= parcela.totalpago;
-        if (Math.abs(valorFinanciado) < 1e-10) {
-          valorFinanciado = 0;
+        // atualiza o saldo
+        saldo -= parcela.totalpago;
+
+        if (Math.abs(saldo) < 1e-10) {
+          saldo = 0;
         }
 
         // se não existir uma movimentação nesse mes, cria uma, se existir, apenas atualiza o valor
         if (!tempo[`${mesParcela}/${anoParcela}`]) {
           tempo[`${mesParcela}/${anoParcela}`] = {
-            saldoDevedor: valorFinanciado,
+            saldoDevedor: saldo,
           };
         } else {
-          tempo[`${mesParcela}/${anoParcela}`].saldoDevedor -=
-            parcela.totalpago;
+          tempo[`${mesParcela}/${anoParcela}`].saldoDevedor = saldo;
 
           if (tempo[`${mesParcela}/${anoParcela}`].saldoDevedor < 1e-10) {
             tempo[`${mesParcela}/${anoParcela}`].saldoDevedor = 0;
@@ -85,6 +109,34 @@ export class FinanceiroService {
     });
     console.log(tempo);
 
-    return JSON.stringify(data);
+    function differenceInMonths(date1, date2) {
+      const year1 = date1.getFullYear();
+      const month1 = date1.getMonth();
+      const year2 = date2.getFullYear();
+      const month2 = date2.getMonth();
+
+      return (year2 - year1) * 12 + (month2 - month1);
+    }
+
+    function addMonths(date, months) {
+      const d = new Date(date);
+      d.setMonth(d.getMonth() + months);
+      return d;
+    }
+
+    let maximoSaldoValor = -Infinity;
+    let maximoSaldoData = '';
+
+    for (const meses in tempo) {
+      if (tempo[meses].saldoDevedor > maximoSaldoValor) {
+        maximoSaldoValor = tempo[meses].saldoDevedor;
+        maximoSaldoData = meses;
+      }
+    }
+
+    return JSON.stringify({
+      mes: maximoSaldoData,
+      total_aberto: maximoSaldoValor,
+    });
   }
 }
